d21925a011129bb40cf6846279f53a423844c2e7
Martha D <45017656+mdempkey@users.noreply.github.com>
2025-10-14 18:23:07 -0700

    About page updated

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,4 +5,12 @@ public class SceneLoader : MonoBehaviour {
     public void LoadGameScene() {
         SceneManager.LoadScene("Scenes/GameScene");
     }
+    
+    public void LoadGameScene1() {
+        SceneManager.LoadScene("Scenes/AboutScreen");
+    }
+    
+    public void LoadGameScene2() {
+        SceneManager.LoadScene("Scenes/MainScreen");
+    }
 }
